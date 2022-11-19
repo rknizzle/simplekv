d0a8c0365eb1f1eb0501e0f02baff55510a916d9
rknizzle <ryan@autotiv.com>
2022-11-19 11:00:16 -0500

    Extract rendezvousHash implementation to its own file

---
+++ rendezvousHash.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"crypto/md5"
+	"sort"
+)
+
+type rendezvousHash struct {
+	nodes []string
+}
+
+func (r rendezvousHash) getNodesForKey(key string, numReplicas int) (nodes []string) {
+	// rendezvous distributed hashing algorithm
+	// https://en.wikipedia.org/wiki/Rendezvous_hashing
+
+	// give a score to each node for the given key
+	var scores sortableNodeScores
+	for _, node := range r.nodes {
+		singleScore := scoreForNode(key, node)
+		ns := nodeScore{node, singleScore}
+		scores = append(scores, ns)
+	}
+
+	// sort the nodes by score
+	sort.Sort(scores)
+
+	// Grab the top numReplica scores
+	var nodesForKey []string
+	for i := 0; i < numReplicas; i++ {
+		nodesForKey = append(nodesForKey, scores[i].node)
+	}
+
+	return nodesForKey
+}
+
+func scoreForNode(key string, node string) []byte {
+	hash := md5.New()
+	hash.Write([]byte(key))
+	hash.Write([]byte(node))
+	score := hash.Sum(nil)
+	return score
+}
+
+// sortable list of nodes by hash score
+type nodeScore struct {
+	// label for the node
+	node string
+	// hash score given to the node for a particular key
+	score []byte
+}
+
+type sortableNodeScores []nodeScore
+
+func (s sortableNodeScores) Len() int      { return len(s) }
+func (s sortableNodeScores) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s sortableNodeScores) Less(i, j int) bool {
+	return bytes.Compare(s[i].score, s[j].score) == 1
+}
--- routing.go
+++ routing.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bytes"
-	"crypto/md5"
 	"fmt"
 	"net/http"
-	"sort"
 )
 
 type distributedHashAlgo interface {
@@ -32,56 +29,4 @@ func (rs routingServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (rs routingServer) getNodesForKey(key string) (nodes []string) {
 	return rs.hash.getNodesForKey(key, rs.numReplicas)
-}
-
-type rendezvousHash struct {
-	nodes []string
-}
-
-func (r rendezvousHash) getNodesForKey(key string, numReplicas int) (nodes []string) {
-	// rendezvous distributed hashing algorithm
-	// https://en.wikipedia.org/wiki/Rendezvous_hashing
-
-	// give a score to each node for the given key
-	var scores sortableNodeScores
-	for _, node := range r.nodes {
-		singleScore := scoreForNode(key, node)
-		ns := nodeScore{node, singleScore}
-		scores = append(scores, ns)
-	}
-
-	// sort the nodes by score
-	sort.Sort(scores)
-
-	// Grab the top numReplica scores
-	var nodesForKey []string
-	for i := 0; i < numReplicas; i++ {
-		nodesForKey = append(nodesForKey, scores[i].node)
-	}
-
-	return nodesForKey
-}
-
-func scoreForNode(key string, node string) []byte {
-	hash := md5.New()
-	hash.Write([]byte(key))
-	hash.Write([]byte(node))
-	score := hash.Sum(nil)
-	return score
-}
-
-// sortable list of nodes by hash score
-type nodeScore struct {
-	// label for the node
-	node string
-	// hash score given to the node for a particular key
-	score []byte
-}
-
-type sortableNodeScores []nodeScore
-
-func (s sortableNodeScores) Len() int      { return len(s) }
-func (s sortableNodeScores) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
-func (s sortableNodeScores) Less(i, j int) bool {
-	return bytes.Compare(s[i].score, s[j].score) == 1
 }
