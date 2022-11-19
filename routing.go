eb2342a3384c9da0bb9d4ee34c3722daf2036082
rknizzle <ryan@autotiv.com>
2022-11-19 10:28:27 -0500

    Add function getNodesForKey
    It determines the nodes that a key should be saved to and accessed from.
    It uses the rendezvous distributed hashing algorithm to determine the
    nodes

--- routing.go
+++ routing.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
 	"fmt"
 	"net/http"
+	"sort"
 )
 
 type routingServer struct {
@@ -18,4 +21,52 @@ func newRoutingServer() routingServer {
 func (rs routingServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// TODO: handle requests here
 	fmt.Println(rs)
+}
+
+func (rs routingServer) getNodesForKey(key string) (nodes []string) {
+	// rendezvous distributed hashing algorithm
+	// https://en.wikipedia.org/wiki/Rendezvous_hashing
+
+	// give a score to each node for the given key
+	var scores sortableNodeScores
+	for _, node := range rs.nodes {
+		singleScore := scoreForNode(key, node)
+		ns := nodeScore{node, singleScore}
+		scores = append(scores, ns)
+	}
+
+	// sort the nodes by score
+	sort.Sort(scores)
+
+	// Grab the top numReplica scores
+	var nodesForKey []string
+	for i := 0; i < rs.numReplicas; i++ {
+		nodesForKey = append(nodesForKey, scores[i].node)
+	}
+
+	return nodesForKey
+}
+
+func scoreForNode(key string, node string) []byte {
+	hash := md5.New()
+	hash.Write([]byte(key))
+	hash.Write([]byte(node))
+	score := hash.Sum(nil)
+	return score
+}
+
+// sortable list of nodes by hash score
+type nodeScore struct {
+	// label for the node
+	node string
+	// hash score given to the node for a particular key
+	score []byte
+}
+
+type sortableNodeScores []nodeScore
+
+func (s sortableNodeScores) Len() int      { return len(s) }
+func (s sortableNodeScores) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s sortableNodeScores) Less(i, j int) bool {
+	return bytes.Compare(s[i].score, s[j].score) == 1
 }
