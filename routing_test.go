6e26e91bbf0692583916c49f89f0f99d74ef35b3
rknizzle <ryan@autotiv.com>
2022-11-20 16:04:26 -0500

    When testing saveValueToKey log that key from all nodes after
    As a way of seeing which nodes got the key written to them, which ones
    didnt and what the value is for the key.
    
    This made me realize that I have a mistake with writing to multiple
    nodes. The reader can only be read from once and so only one of the
    nodes will get the value written to them. So I need to split the reader
    into a multireader if I want to send it to be written to multiple nodes

--- routing_test.go
+++ routing_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -34,5 +36,28 @@ func TestSaveValueToKey(t *testing.T) {
 		t.Fatalf("Failed to save the value for key: %s with message: %s", key, err.Error())
 	}
 
-	// TODO: iterate through the nodes and check which/how many got the the value written to them
+	// verify that the value was written to the expected number of nodes
+	nodes := rs.hash.getAllNodes()
+
+	for i, node := range nodes {
+		// log the value for the key on each node (even if it didnt get written to it)
+		valReader, err := node.get(key)
+		if err != nil {
+			if strings.Contains(err.Error(), "doesnt exist") {
+				fmt.Printf("Node %d: this node doesnt have the key & value saved\n", i)
+			} else {
+				fmt.Printf("Node %d error trying to get the value with message: %s\n", i, err.Error())
+			}
+			continue
+		}
+
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(valReader)
+		fmt.Printf("Node %d: %s\n", i, buf.String())
+	}
+
+	// TODO ISSUE: saveValueToKey correctly tried to write the key & value to 2 nodes but the value
+	// was only actually written to one of the nodes. The issue is because theres only one read stream
+	// so I need to split it into a read stream for each replica
+
 }
