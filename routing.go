a8fda05e8ffdf9a22cde3f2a71bc2f1ddc271668
rknizzle <ryan@autotiv.com>
2022-11-20 10:37:27 -0500

    Add an outline for a saveValueToKey method on the routingServer
    This wont currently work but its just a potential contract for this
    function. To get this to work I think Im going to need to change to
    using a list of Node types instead of just a list of string labels
    
    The Node will probably contain a label and a storageEngine

--- routing.go
+++ routing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -29,4 +30,20 @@ func (rs routingServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (rs routingServer) getNodesForKey(key string) (nodes []string) {
 	return rs.hash.getNodesForKey(key, rs.numReplicas)
+}
+
+func (rs routingServer) saveValueToKey(key string, value io.Reader) error {
+	// TODO: should this return a slice of nodes rather than a slice of strings labels for nodes?
+	nodes := rs.getNodesForKey(key)
+
+	for _, node := range nodes {
+		// TODO: probably do these concurrently / in parallel
+		// NOTE: this could just be a call to save to a map OR an HTTP request depending on the type
+		err := node.write(key, value)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
