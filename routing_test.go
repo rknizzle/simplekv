6ff7e63e5b4d9a7f223ef37dbead9dd5a1a67004
rknizzle <ryan@autotiv.com>
2022-11-20 11:17:44 -0500

    Add a basic test for saveValueToKey

---
+++ routing_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSaveValueToKey(t *testing.T) {
+	rh := rendezvousHash{
+		nodes: []storageNode{
+			storageNode{
+				label:         "localhost:3000",
+				storageEngine: newInmemoryStorage(),
+			},
+			storageNode{
+				label:         "localhost:3001",
+				storageEngine: newInmemoryStorage(),
+			},
+			storageNode{
+				label:         "localhost:3002",
+				storageEngine: newInmemoryStorage(),
+			},
+		},
+	}
+
+	rs := newRoutingServer(rh)
+
+	key := "hello"
+	value := "world"
+	valueReader := strings.NewReader(value)
+
+	err := rs.saveValueToKey(key, valueReader)
+	if err != nil {
+		t.Fatalf("Failed to save the value for key: %s with message: %s", key, err.Error())
+	}
+
+	// TODO: iterate through the nodes and check which/how many got the the value written to them
+}
