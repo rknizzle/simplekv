035e92ea8365662616edc76400c7220dd939741e
rknizzle <ryan@autotiv.com>
2022-11-22 18:18:00 -0500

    Initialize the app with some test data in the nodes
    This is just for testing out reads for write now

--- main.go
+++ main.go
@@ -9,7 +9,35 @@ func main() {
 	// setup the routing server based on the config
 	port := 8080
 
-	rh := rendezvousHash{}
+	key := "hello"
+	value := "world"
+
+	// preload a key in the nodes just for testing
+	rh := rendezvousHash{
+		nodes: []storageNode{
+			testStorageNode{
+				label: "localhost:3000",
+				storageEngine: inmemoryStorage{
+					storageMap: map[string][]byte{
+						key: []byte(value),
+					},
+				},
+			},
+			testStorageNode{
+				label:         "localhost:3001",
+				storageEngine: newInmemoryStorage(),
+			},
+			testStorageNode{
+				label: "localhost:3002",
+				storageEngine: inmemoryStorage{
+					storageMap: map[string][]byte{
+						key: []byte(value),
+					},
+				},
+			},
+		},
+	}
+
 	rs := newRoutingServer(2, rh)
 	api := newRestAPI(rs)
 
