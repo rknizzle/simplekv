12e8f388dee498c382fd1b42fc73ba5d39b26628
rknizzle <ryan@autotiv.com>
2022-11-22 19:00:08 -0500

    Add an HTTP api for the storage nodes

---
+++ storageHTTP.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+type storageRESTapi struct {
+	se storageEngine
+}
+
+func newStorageRESTapi(se storageEngine) storageRESTapi {
+	return storageRESTapi{se: se}
+}
+
+func (api storageRESTapi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	key := getKeyFromURL(r.URL.Path)
+
+	if r.Method == "GET" {
+		valueReader, err := api.se.get(key)
+		if err != nil {
+			respondWithError(err.Error(), w)
+			return
+		}
+
+		// stream the value to the response
+		io.Copy(w, valueReader)
+
+	} else if r.Method == "POST" {
+		err := api.se.write(key, r.Body)
+		if err != nil {
+			respondWithError(err.Error(), w)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write(nil)
+	}
+}
