e99ce4ba57d62e0ab556ba47f31adf913b3f1fba
rknizzle <ryan@autotiv.com>
2022-11-22 19:48:43 -0500

    Add HTTP tests for the storage API

---
+++ storageHTTP_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestStorageHTTPgetSuccessful(t *testing.T) {
+
+	// preload the node with the key 'hello'
+	storageEngine := newInmemoryStorage()
+	storageEngine.storageMap["hello"] = []byte("world")
+
+	api := newStorageRESTapi(storageEngine)
+
+	// get the hello key
+	req, err := http.NewRequest("GET", "/hello", nil)
+	if err != nil {
+		t.Fatalf("Failed to create the new request")
+	}
+
+	rec := httptest.NewRecorder()
+
+	api.ServeHTTP(rec, req)
+
+	if rec.Code != http.StatusOK {
+		t.Fatalf("Expected 200 code but got %d", rec.Code)
+	}
+
+	if rec.Body.String() != "world" {
+		t.Fatalf("Expected the response body to contain world but got %s", rec.Body.String())
+	}
+}
+
+func TestStorageHTTPgetMissingKey(t *testing.T) {
+	storageEngine := newInmemoryStorage()
+	api := newStorageRESTapi(storageEngine)
+
+	req, err := http.NewRequest("GET", "/hello", nil)
+	if err != nil {
+		t.Fatalf("Failed to create the new request")
+	}
+
+	rec := httptest.NewRecorder()
+
+	api.ServeHTTP(rec, req)
+
+	if rec.Code != http.StatusInternalServerError {
+		t.Fatalf("Expected 500 code but got %d", rec.Code)
+	}
+
+	// TODO: standardize  this error message with the routing servers
+	if !strings.Contains(rec.Body.String(), "doesnt exist") {
+		t.Fatalf("Expected an error about the key not existing but instead got %s", rec.Body.String())
+	}
+}
+
+func TestStorageHTTPwriteSuccessful(t *testing.T) {
+	storageEngine := newInmemoryStorage()
+	api := newStorageRESTapi(storageEngine)
+
+	reqBody := strings.NewReader("world")
+	req, err := http.NewRequest("POST", "/hello", reqBody)
+	if err != nil {
+		t.Fatalf("Failed to create the new request")
+	}
+
+	rec := httptest.NewRecorder()
+
+	api.ServeHTTP(rec, req)
+
+	if rec.Code != http.StatusCreated {
+		t.Fatalf("Expected 201 code but got %d", rec.Code)
+	}
+}
