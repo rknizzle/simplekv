768f78803b766b7e550d3ae903c9a5004dd25406
rknizzle <ryan@autotiv.com>
2022-11-24 08:20:51 -0500

    Add a cmd entrypoint for the routing and storage server

---
+++ cmd/routingServer/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/rknizzle/simplekv/pkg/routing"
+	"github.com/rknizzle/simplekv/pkg/storage"
+)
+
+func main() {
+	// setup the routing server based on the config
+	port := 8080
+
+	key := "hello"
+	value := "world"
+
+	// preload a key in the nodes just for testing
+	rh := routing.RendezvousHash{
+		Nodes: []storage.StorageNode{
+			storage.TestStorageNode{
+				Label: "localhost:3000",
+				StorageEngine: storage.InmemoryStorage{
+					StorageMap: map[string][]byte{
+						key: []byte(value),
+					},
+				},
+			},
+			storage.TestStorageNode{
+				Label:         "localhost:3001",
+				StorageEngine: storage.NewInmemoryStorage(),
+			},
+			storage.TestStorageNode{
+				Label: "localhost:3002",
+				StorageEngine: storage.InmemoryStorage{
+					StorageMap: map[string][]byte{
+						key: []byte(value),
+					},
+				},
+			},
+		},
+	}
+
+	rs := routing.NewRoutingServer(2, rh)
+	api := routing.NewRestAPI(rs)
+
+	http.ListenAndServe(fmt.Sprintf(":%d", port), api)
+}
---
+++ cmd/storageServer/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/rknizzle/simplekv/pkg/storage"
+)
+
+func main() {
+	storageEngine := storage.NewInmemoryStorage()
+	api := storage.NewStorageRESTapi(storageEngine)
+
+	port := 8000
+	http.ListenAndServe(fmt.Sprintf(":%d", port), api)
+}
