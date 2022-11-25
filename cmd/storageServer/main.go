5c9a39afbc728028678ee170e6e7d9f51d1c4c40
rknizzle <ryan@autotiv.com>
2022-11-25 10:34:44 -0500

    Refactor: Extract function getPortFromEnv

--- cmd/storageServer/main.go
+++ cmd/storageServer/main.go
@@ -13,17 +13,26 @@ func main() {
 	storageEngine := storage.NewFileSystemStorage()
 	api := storage.NewStorageRESTapi(storageEngine)
 
+	port, err := getPortFromEnv()
+	if err != nil {
+		panic(err)
+	}
+
+	http.ListenAndServe(fmt.Sprintf(":%d", port), api)
+}
+
+func getPortFromEnv() (int, error) {
 	var port int
 	var err error
 	envVar, present := os.LookupEnv("PORT")
 	if present {
 		port, err = strconv.Atoi(envVar)
 		if err != nil {
-			panic(err)
+			return 0, err
 		}
 	} else {
 		port = 8000
 	}
 
-	http.ListenAndServe(fmt.Sprintf(":%d", port), api)
+	return port, nil
 }
