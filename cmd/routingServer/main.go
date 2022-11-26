6f29ea07128c8598be2b0d7217f9d65640556e39
rknizzle <ryan@autotiv.com>
2022-11-26 18:26:17 -0500

    Modify the entrypoint of the routingServer to start with remoteNodes
    Ill need to eventually modify this to be configurable for your storage
    nodes but for now this will help for testing with a docker compose
    environment for testing multiple nodes running separately

--- cmd/routingServer/main.go
+++ cmd/routingServer/main.go
@@ -5,39 +5,17 @@ import (
 	"net/http"
 
 	"github.com/rknizzle/simplekv/pkg/routing"
-	"github.com/rknizzle/simplekv/pkg/storage"
 )
 
 func main() {
 	// setup the routing server based on the config
 	port := 8080
 
-	key := "hello"
-	value := "world"
-
-	// preload a key in the nodes just for testing
 	rh := routing.RendezvousHash{
 		Nodes: []routing.StorageNode{
-			routing.TestStorageNode{
-				Label: "localhost:3000",
-				StorageEngine: storage.InmemoryStorage{
-					StorageMap: map[string][]byte{
-						key: []byte(value),
-					},
-				},
-			},
-			routing.TestStorageNode{
-				Label:         "localhost:3001",
-				StorageEngine: storage.NewInmemoryStorage(),
-			},
-			routing.TestStorageNode{
-				Label: "localhost:3002",
-				StorageEngine: storage.InmemoryStorage{
-					StorageMap: map[string][]byte{
-						key: []byte(value),
-					},
-				},
-			},
+			routing.RemoteStorageNode{URL: "http://storage_server_1:8000"},
+			routing.RemoteStorageNode{URL: "http://storage_server_2:8000"},
+			routing.RemoteStorageNode{URL: "http://storage_server_3:8000"},
 		},
 	}
 
