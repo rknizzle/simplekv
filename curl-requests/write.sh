b28dc55aac74d05121e623304289750371310ad3
rknizzle <ryan@autotiv.com>
2022-11-26 18:43:21 -0500

    Modify write curl request to just upload the file content
    Rather than sending up a form containing a file upload

--- curl-requests/write.sh
+++ curl-requests/write.sh
@@ -1,3 +1,5 @@
 key_name=$1
 
-curl -i -F upload=@./curl-requests/testData.txt http://localhost:8080/$key_name
+curl -i -X POST -H 'Content-type: text/plain' \
+  --data-binary @./curl-requests/testData.txt \
+  http://localhost:8080/$key_name
