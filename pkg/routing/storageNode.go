82cf354eef4b3f630cf11895ee34f388355221dc
rknizzle <ryan@autotiv.com>
2022-11-25 21:38:15 -0500

    Add a RemoteStorageNode type that implements StorageNode
    It just sends an HTTP request to a storageNode that is a separate
    machine

--- pkg/routing/storageNode.go
+++ pkg/routing/storageNode.go
@@ -1,7 +1,10 @@
 package routing
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/rknizzle/simplekv/pkg/storage"
 )
@@ -27,4 +30,50 @@ func (s TestStorageNode) Get(key string) (io.Reader, error) {
 
 func (s TestStorageNode) GetLabel() (label string) {
 	return s.Label
+}
+
+type RemoteStorageNode struct {
+	URL string
+}
+
+func (s RemoteStorageNode) Write(key string, value io.Reader) error {
+	url := fmt.Sprintf("%s/%s", s.URL, key)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url, value)
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	_, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	// TODO: parse out response to check for errors
+
+	return nil
+}
+
+func (s RemoteStorageNode) Get(key string) (io.Reader, error) {
+	url := fmt.Sprintf("%s/%s", s.URL, key)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	//defer resp.Body.Close()
+	// TODO: how do I make sure that the response gets closed later?
+
+	return resp.Body, nil
+}
+
+func (s RemoteStorageNode) GetLabel() (label string) {
+	return s.URL
 }
