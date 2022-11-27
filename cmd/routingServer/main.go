6c001b4e05eee52d797e6b5eddc611b8c536a86a
rknizzle <ryan@autotiv.com>
2022-11-26 20:05:07 -0500

    Add flags to routign server entrypoint
    Added --storage for specifying a URL of a storage node (use it multiple
    times to start with multiple storage nodes)
    And also added a --replicas flag

--- cmd/routingServer/main.go
+++ cmd/routingServer/main.go
@@ -1,26 +1,52 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 
 	"github.com/rknizzle/simplekv/pkg/routing"
 )
 
+type storageNodeFlags []string
+
+func (s *storageNodeFlags) String() string { return "" }
+func (s *storageNodeFlags) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
+
 func main() {
 	// setup the routing server based on the config
 	port := 8080
 
+	replicas := flag.Int("replicas", 2, "Number of replicas to save data to")
+
+	var storageNodeURLs storageNodeFlags
+	flag.Var(&storageNodeURLs, "storage", "The URL of a storage node")
+
+	flag.Parse()
+
+	storageNodes := URLsToStorageNodes(storageNodeURLs)
+
 	rh := routing.RendezvousHash{
-		Nodes: []routing.StorageNode{
-			routing.RemoteStorageNode{URL: "http://storage_server_1:8000"},
-			routing.RemoteStorageNode{URL: "http://storage_server_2:8000"},
-			routing.RemoteStorageNode{URL: "http://storage_server_3:8000"},
-		},
+		Nodes: storageNodes,
 	}
 
-	rs := routing.NewRoutingServer(2, rh)
+	rs := routing.NewRoutingServer(*replicas, rh)
 	api := routing.NewRestAPI(rs)
 
 	http.ListenAndServe(fmt.Sprintf(":%d", port), api)
+}
+
+func URLsToStorageNodes(urls []string) []routing.StorageNode {
+	var storageNodes []routing.StorageNode
+	for _, url := range urls {
+		s := routing.RemoteStorageNode{
+			URL: url,
+		}
+		storageNodes = append(storageNodes, s)
+	}
+
+	return storageNodes
 }
